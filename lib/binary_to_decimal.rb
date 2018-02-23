eca87deef23c6fb7a3723e9738afb3073794e3a3
Kaitlin Forsman <kcforsman@outlook.com>
2018-02-22 20:18:09 -0800

    finished first working version of conversion from binary to decimal method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,24 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+require 'awesome_print'
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_result = 0
+  length = binary_array.length
+  binary_array.length.times do |x|
+    decimal_result += (binary_array[length - 1] * 2**(x))
+    length -= 1
+  end
+  decimal_result
 end
+
+# # testing method
+# ap binary_to_decimal([1, 0, 1, 1, 0, 0, 0, 1])
+# ap "10110001".to_i(2)
+#
+# ap binary_to_decimal([1, 0, 0, 1, 0])
+# ap "10010".to_i(2)
+#
+# ap binary_to_decimal([1, 0 , 1])
+# ap "101".to_i(2)
