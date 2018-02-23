ad5c50d0fdea6011d167b42724f50f6254ed28bc
Kat <kat.eastman@gmail.com>
2018-02-22 22:26:14 -0800

    runs without errors using a times loop as as instructed.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,10 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  #step one
+  total = 0
+  8.times do |i|
+    total += (binary_array[-(i + 1)] * 2 ** i)
+  end
+  return total
 end
