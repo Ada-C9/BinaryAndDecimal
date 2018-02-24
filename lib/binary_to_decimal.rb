a8c6d3e26df5955c1f7c968e314a3a37c1ba3464
Cara <cararbrennan@gmail.com>
2018-02-24 13:35:13 -0800

    Method to convert binary to decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,16 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  n = 0
+  decimal_number = 0
+  #moves through each element of the binary array
+  binary_array.length.times do
+    #finds number to add to the total sum for decimal number
+    num_to_sum = (binary_array[-1] * 2) ** n
+    binary_array.delete_at(-1)
+    n += 1
+    decimal_number += num_to_sum
+  end
+  #returns the number in base 10
+  return "#{decimal_number} base 10"
 end
