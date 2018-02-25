d2a663f11fc47ebbfd4309ae7eb0c8aed4cbb906
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-25 13:26:34 -0800

    created method to return decimal from binary

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,15 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+
+#Creates randomly filled array with 0's and 1's, 8 times.
+binary_array = []
+8.times do
+  binary_array << rand(0..1)
+end
+
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+
 end
