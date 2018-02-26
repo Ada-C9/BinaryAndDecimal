b2732b0121bc55e5e29f9164ece08356b0dcd06e
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-02-25 23:34:18 -0800

    added binary_to_decimal method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,46 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+
 def binary_to_decimal(binary_array)
+
+  decimal_value = 0
+
+  if binary_array[(0)] == 1
+    decimal_value =  decimal_value + 128
+  end
+
+  if binary_array[(1)] == 1
+    decimal_value =  decimal_value +  64
+  end
+
+  if binary_array[(2)] == 1
+    decimal_value =  decimal_value + 32
+  end
+
+  if binary_array[(3)] == 1
+    decimal_value =  decimal_value + 16
+  end
+
+  if binary_array[(4)] == 1
+    decimal_value =  decimal_value + 8
+  end
+
+  if binary_array[(5)] == 1
+    decimal_value =  decimal_value + 4
+  end
+
+  if binary_array[(6)] == 1
+    decimal_value =  decimal_value + 2
+  end
+
+  if binary_array[(7)] == 1
+    decimal_value =  decimal_value + 1
+  end
+
+  return decimal_value
+
   raise NotImplementedError
+
 end
