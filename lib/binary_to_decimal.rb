09ab16a593c8f7537b24c59b5ef2de31fa7456d6
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-02-25 19:09:01 -0800

    Create method and pass tests

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,17 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
+# Create a random binary array
+
+binary_array = []
+8.times { binary_array << rand(0..1)}
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  # Reverse the array to be able to use it in the loop for calculations
+  binary_array = binary_array.reverse
+  decimal = 0
+  # Loop over the indexes in the array and multiply them by 2 at the power of the index used
+  8.times {|i| decimal += binary_array[i] * (2 ** i)}
+  return decimal
 end
