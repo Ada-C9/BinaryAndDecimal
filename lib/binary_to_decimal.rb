372f17a49f3046f2d5ec9dc22c886e23ed48c08d
Madaleine Shields <maddiev.shields@gmail.com>
2018-02-26 15:51:04 -0800

    Finished code

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,9 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
 # A method named `binary_to_decimal` that receives as input an array of size 8.
 # The array is randomly filled with 0’s and 1’s.
 # The most significant bit is at index 0.
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
-def binary_to_decimal(binary_array)
-  raise NotImplementedError
+#Creates randomly filled array with 0's and 1's, 8 times.
+
+#Create method named binary_to_decimal to return decimal value. input is binary array.
+
+binary = []
+8.times do
+  binary << rand(0..1)
 end
+
+def binary_to_decimal(binary_array)
+  subtotals = []
+  p binary_array
+
+  binary_array.length.times do |i|
+    subtotals << binary_array[i] * ((2) ** ((binary_array.length - 1) - i))
+  end
+
+  decimal_number = 0
+  subtotals.each { |subtotal| decimal_number += subtotal }
+  # p decimal_number
+  # return decimal_number
+end
+
+decimal_number = binary_to_decimal(binary)
+return decimal_number
