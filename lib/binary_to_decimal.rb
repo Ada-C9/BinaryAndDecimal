3b47a16f8ab97560e62ced73b42dfb2f1750fa0a
Mariko <marikoanderson@gmail.com>
2018-02-27 23:39:31 -0800

    wrote code for binary to decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,23 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+require 'awesome_print'
+#
+# binary_array = []
+#
+# 8.times do |rand_num|
+#   binary_array << ([0, 1].sample)
+# end
+# ap binary_array
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+
+  decimal = 0
+  binary_array.length.times do |i|
+    add_to_decimal = binary_array[i] * (2**(binary_array.length - i - 1))
+    decimal += add_to_decimal
+    
+  end
+  return decimal
+  # raise NotImplementedError
 end
